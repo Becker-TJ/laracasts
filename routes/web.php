4ac3fb5199623dd27f961de02db52f2df9a63db2
TJ <tjbecker.okc@gmail.com>
2022-09-26 17:12:51 -0500

    First Vue Lesson

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/vue-learning', function () {
+    return view('vue-learning');
+});
